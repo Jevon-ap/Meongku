40c6547f0f39a39574c06de504f1d969923bf669
jevon-ap <jevonnetran@gmail.com>
2023-06-12 10:52:34 +0700

    update fungsi camera sudah tapi gallery blm

--- app/src/main/java/com/example/meongku/ui/main/scan/ScanActivity.kt
+++ app/src/main/java/com/example/meongku/ui/main/scan/ScanActivity.kt
@@ -1,11 +1,15 @@
 package com.example.meongku.ui.main.scan
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -24,7 +28,10 @@ class ScanActivity : AppCompatActivity() {
         binding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.captureImage.setOnClickListener { takePhoto() }
+        binding.captureGallery.setOnClickListener { startGallery() }
+        binding.captureImage.setOnClickListener {
+            takePhoto()
+        }
         binding.SwitchCamera.setOnClickListener {
             cameraSelector = if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)) CameraSelector.DEFAULT_FRONT_CAMERA
             else CameraSelector.DEFAULT_BACK_CAMERA
@@ -38,6 +45,7 @@ class ScanActivity : AppCompatActivity() {
         startCamera()
     }
     private fun takePhoto() {
+        Log.d("ScanActivity", "Taking photo...")
         val imageCapture = imageCapture ?: return
         val photoFile = createFile(application)
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
@@ -46,6 +54,7 @@ class ScanActivity : AppCompatActivity() {
             ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
+                    Log.d("ScanActivity", "Error taking photo: ${exc.localizedMessage}")
                     Toast.makeText(
                         this@ScanActivity,
                         "Gagal mengambil gambar.",
@@ -53,18 +62,29 @@ class ScanActivity : AppCompatActivity() {
                     ).show()
                 }
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    Toast.makeText(
-                        this@ScanActivity,
-                        "Berhasil mengambil gambar.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Log.d("ScanActivity", "Photo saved.")
+                    val myFile = photoFile
+                    startScanResultActivity(myFile.absolutePath, cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+
                 }
             }
         )
     }
 
-    private fun startCamera() {
+    private fun startScanResultActivity(imagePath: String, isBackCamera: Boolean) {
+        val resultIntent = Intent(this@ScanActivity, ScanResultActivity::class.java).apply {
+            putExtra("picture", imagePath)
+            putExtra("isBackCamera", isBackCamera)
+        }
+        try {
+            startActivity(resultIntent)
+        } catch (e: Exception) {
+            Log.d("ScanActivity", "Error starting ScanResultActivity: ${e.localizedMessage}")
+        }
+    }
 
+    private fun startCamera() {
+        Log.d("ScanActivity", "Starting camera...")
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
@@ -86,6 +106,7 @@ class ScanActivity : AppCompatActivity() {
                     imageCapture
                 )
             } catch (exc: Exception) {
+                Log.d("ScanActivity", "Error starting camera: ${exc.localizedMessage}")
                 Toast.makeText(
                     this@ScanActivity,
                     "Gagal memunculkan kamera.",
@@ -106,5 +127,32 @@ class ScanActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun startGallery() {
+        Log.d("ScanActivity", "Starting gallery...")
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            Log.d("ScanActivity", "Image selected from gallery.")
+            val selectedImg = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@ScanActivity)
+            val resultIntent = Intent(this@ScanActivity, ScanResultActivity::class.java).apply {
+                putExtra("picture", myFile)
+            }
+            try {
+                startActivity(resultIntent)
+            } catch (e: Exception) {
+                Log.d("ScanActivity", "Error starting ScanResultActivity with image from gallery: ${e.localizedMessage}")
+            }
+        }
     }
 }
